bee30317f7277566e4de01ac5a68650f9b3bf979
al1kite <wjdekdus62@naver.com>
2024-03-17 22:34:07 +0900

    [add] 프로젝트 생성

---
+++ src/main/java/com/clue_b/ClueBApplication.java
@@ -0,0 +1,13 @@
+package com.clue_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClueBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClueBApplication.class, args);
+    }
+
+}
